26dd9395afe37d6640f521b88b8f5c8aa341e795
Michael Nelson <revnelson@gmail.com>
2023-06-23 22:25:53 -0700

    Use Promise.all in layout to reduce loading time.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,5 +2,8 @@ import { load_GetCart, load_GetProducts } from '$houdini';
 import type { ServerLoad } from '@sveltejs/kit';
 
 export const load: ServerLoad = async (event) => {
-	return { ...(await load_GetProducts({ event })), ...(await load_GetCart({ event })) };
+	const productsPromise = await load_GetProducts({ event });
+	const cartPromise = await load_GetCart({ event });
+	const [productsStore, cartStore] = await Promise.all([productsPromise, cartPromise]);
+	return { ...productsStore, ...cartStore };
 };
